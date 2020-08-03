fbae49d94b6ff8b672a9c8b29c7540a3bee7f16a
jingdd-boop <1736929925@qq.com>
2020-08-03 15:22:56 +0800

    js

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -21,3 +21,13 @@ var removeElement = function(nums,val) {
   }
   return ans;
 }
+
+var removeElement = function(nums, val) {
+  for(let i=0;i<nums.length;i++){
+      if(nums[i] == val){
+          nums.splice(i,1)
+          i--;
+      }
+  }
+  return nums.length
+};
