ec1b0a63cb10f759e00a300b5c3774920c0814a8
jingdd-boop <1736929925@qq.com>
2020-08-04 21:07:18 +0800

    js

---
+++ 数组扁平化，去重，排序.js
@@ -0,0 +1,10 @@
+var arr = [[1,2,2],[3,4,5,5],[6,7,8,9,[11,12,[14]]],10]
+//扁平化
+var flatArr = arr.flat(4);
+//去重
+var disArr = Array.from(new Set(flatArr))
+//排序
+let result = disArr.sort(function(a,b){
+  return a-b
+})
+console.log(result);
