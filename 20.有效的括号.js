fa983577e5f98ec8f740d23df48afa21f9afca1b
jingdd-boop <1736929925@qq.com>
2020-08-06 14:31:18 +0800

    code

---
+++ 20.有效的括号.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let arr = []
+  let len = s.length
+  if (len%2) return false
+  for (let i = 0; i < len; i++) {
+      let letter = s[i]
+      switch(letter) {
+          case "(": {
+              arr.push(letter)
+              break;
+          }
+          case "[": {
+              arr.push(letter)
+              break;
+          }
+          case "{": {
+              arr.push(letter)
+              break;
+          }
+          case ")": {
+              if (arr.pop() !== "(") return false
+              break;
+          }
+          case "]": {
+               if (arr.pop() !== "[") return false
+              break;
+          }
+          case "}": {
+              if (arr.pop() !== "{") return false
+              break;
+          }
+      }
+  }
+  return !arr.length
+
+};
+
