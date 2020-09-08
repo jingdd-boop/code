413452b6d4a57138c92b0bb4c293af7250486bf6
jingdd-boop <1736929925@qq.com>
2020-09-08 14:09:27 +0800

    code

---
+++ 234.回文链表.js
@@ -0,0 +1,30 @@
+var isPalindrome = function(head) {
+  if(head === null || head.next === null) return true;
+  let mid = head;
+  let pre = null;
+  let reversed = null;
+  //end 每次走两步，这个循环的时间复杂度为O(n/2)
+  //循环条件：只要head存在则最少走一次
+  while(head !== null && head.next !== null){
+    
+    pre = mid;//这个赋值要在mid被修改前提前
+
+    //遍历链表
+    mid = mid.next;//中间指针一次走一格  
+    head = head.next.next;//指针一次走两个
+
+    //通过迭代来反转前半部分的链表，
+    //反转前面部分的节点，并用reversed保存
+    pre.next = reversed;
+    reversed = pre;
+  }
+
+  //奇数情况分开讨论
+  if(head) mid = mid.next;
+  while(mid){
+    if(reversed.val !== mid.val) return false;
+    reversed = reversed.next;
+    mid = mid.next;
+  }
+  return true;
+}
