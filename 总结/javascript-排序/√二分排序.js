883ae362d7a1f37096e9d2cd2b40c1cd14826ae4
jingdd-boop <1736929925@qq.com>
2020-09-25 16:24:32 +0800

    排序

---
+++ 总结/javascript-排序/√二分排序.js
@@ -0,0 +1,42 @@
+// function sort(arr){
+//   if(arr.length <=1){
+//     return arr;
+//   }
+//   var left = [];
+//   var right = [];
+//   var midIndex = parseInt(arr.length /2);
+//   var mid = arr[midIndex];
+//   for(var i = 0;i<arr.length;i++){
+//     if(i == midIndex) continue;
+//     if(arr[i] < mid){
+//       left.push(arr[i])
+//     }else{
+//       right.push(arr[i]);
+//     }
+//   }
+//   return sort(left).concat([mid],sort(right));
+// }
+
+function sort(arr){
+  if(arr.length < 1){
+    return arr;
+  }
+  var left = [];
+  var right = [];
+  var midIndex = parseInt(arr.length /2);
+  var mid = arr[midIndex];
+  for(var i = 0;i<arr.length;i++){
+    if(i == midIndex) continue;//在中间元素的两边找就可以了
+
+    if(arr[i] < mid){
+      left.push(arr[i])
+    }
+    else{
+      right.push(arr[i])
+    }
+  } 
+   return sort(left).concat([mid],sort(right));
+}
+
+arr = [3,5,7,3,5,8,1,9,23,45,31]
+console.log(sort(arr));
---
+++ 总结/javascript-排序/√插入排序.js
@@ -0,0 +1,29 @@
+// var arr = [34,54,1,3,24,8,92,32];
+// for(var i = 0;i<arr.length;i++){
+//   var n = i;
+//   while(arr[n]>arr[n+1] && n>=0){
+//     var tem =arr[n];
+//     arr[n]=arr[n+1];
+//     arrr[n+1]=temp;
+//     n--
+//   }
+// }
+
+function sort(arr){
+  for(var i = 0;i<arr.length;i++){
+    var n =i;
+    while(arr[n] > arr[n+1] && n>=0){
+      var t = arr[n];
+      arr[n] = arr[n+1];
+      arr[n+1] = t;
+      n--
+    }
+  }
+  return arr;
+}
+
+var arr = [1,2,6,4,8,7];
+console.log(sort(arr));
+//插入排序
+//时间复杂度：O(n*n)
+//空间复杂度:O(1)
