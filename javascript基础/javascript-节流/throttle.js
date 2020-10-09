078b8ec99074e7b7e33f3dd3257cf8b27254d0b0
jingdd-boop <1736929925@qq.com>
2020-10-09 16:45:46 +0800

    练习

--- javascript基础/javascript-节流/throttle.js
+++ javascript基础/javascript-节流/throttle.js
@@ -72,25 +72,51 @@ function throttle(func,wait){
   }
 }
 
+//时间戳版10/9
+function throttle(func,wait){
+  var context,args;
+  var previous = 0;
+  return function(){
+    var now = +new Date();
+    context = this;
+    args = arguments;
+    if(now - previous > wait){
+      func.apply(context,args);
+      previous = now;
+    }
+  }
+}
 
+定时器版
+function throttle(func, wait) {
+  var timeout;
+  return function() {
+      context = this;
+      args = arguments;
+      if (!timeout) {
+          timeout = setTimeout(function(){
+              timeout = null;
+              func.apply(context, args)
+          }, wait)
+      }
 
-// 定时器版
-// function throttle(func, wait) {
-//   var timeout;
-//   var previous = 0;
+  }
+}
 
-//   return function() {
-//       context = this;
-//       args = arguments;
-//       if (!timeout) {
-//           timeout = setTimeout(function(){
-//               timeout = null;
-//               func.apply(context, args)
-//           }, wait)
-//       }
-
-//   }
-// }
+//10/9
+function throttle(func,wait) {
+  var timeout;
+  return function() {
+    context = this;
+    args = arguments;
+    if(!timeout) {
+      timeout = setTimeout(function() {
+        timeout = null;
+        func.apply(context,args)
+      },wait)
+    }
+  }
+}
 //双剑合璧版
 function throttle(func,wait){
   var timeout,context,args,result;
