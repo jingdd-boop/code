41cbb06160089addc0ba965f1b72fe58256145ab
jingdd-boop <1736929925@qq.com>
2020-11-21 13:43:23 +0800

    83.删除排序链表的重复元素

---
+++ 链表/83. 删除排序链表中的重复元素.js
@@ -0,0 +1,16 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+ var delectnode = function(head) {
+   let cur = head;
+   while(cur != null && cur.next != null) {
+     if (cur.val === cur.next.val) {
+       cur.next = cur.next.next;
+     } else {
+       cur = cur.next;
+     }
+   }
+   return head;
+ }
