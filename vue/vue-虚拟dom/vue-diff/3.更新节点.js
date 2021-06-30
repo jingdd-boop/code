837354996cc18fa85397c418616ae690fc28dab4
longjing <1736929925@qq.com>
2021-06-30 11:26:11 +0800

    vue-diff

---
+++ vue/vue-虚拟dom/vue-diff/3.更新节点.js
@@ -0,0 +1,86 @@
+// 更新节点的函数
+function patchVnode ( // 一些参数
+    oldVnode, // 旧节点
+    vnode, // 新节点
+    insertedVnodeQueue, //
+    ownerArray,
+    index,
+    removeOnly
+  ) {
+    // 如果新旧节点一样，直接return 不需要操作
+    if (oldVnode === vnode) {
+      return
+    }
+
+    if (isDef(vnode.elm) && isDef(ownerArray)) {
+      // clone reused vnode
+      vnode = ownerArray[index] = cloneVNode(vnode)
+    }
+
+    const elm = vnode.elm = oldVnode.elm
+
+    if (isTrue(oldVnode.isAsyncPlaceholder)) {
+      if (isDef(vnode.asyncFactory.resolved)) {
+        hydrate(oldVnode.elm, vnode, insertedVnodeQueue)
+      } else {
+        vnode.isAsyncPlaceholder = true
+      }
+      return
+    }
+
+    // reuse element for static trees.
+    // note we only do this if the vnode is cloned -
+    // if the new node is not cloned it means the render functions have been
+    // reset by the hot-reload-api and we need to do a proper re-render.
+    // 如果新旧节点是静态节点
+    if (isTrue(vnode.isStatic) &&
+      isTrue(oldVnode.isStatic) &&
+      vnode.key === oldVnode.key &&
+      (isTrue(vnode.isCloned) || isTrue(vnode.isOnce))
+    ) {
+      vnode.componentInstance = oldVnode.componentInstance
+      return
+    }
+
+    let i
+    const data = vnode.data
+    if (isDef(data) && isDef(i = data.hook) && isDef(i = i.prepatch)) {
+      i(oldVnode, vnode)
+    }
+
+    const oldCh = oldVnode.children
+    const ch = vnode.children
+    if (isDef(data) && isPatchable(vnode)) {
+      for (i = 0; i < cbs.update.length; ++i) cbs.update[i](oldVnode, vnode)
+      if (isDef(i = data.hook) && isDef(i = i.update)) i(oldVnode, vnode)
+    }
+     // vnode有text属性？若没有
+    if (isUndef(vnode.text)) {
+      // vnode的子节点与oldVnode的子节点是否都存在？
+      if (isDef(oldCh) && isDef(ch)) {
+        // 若都存在，判断子节点是否相同，不同则更新子节点
+        if (oldCh !== ch) updateChildren(elm, oldCh, ch, insertedVnodeQueue, removeOnly)
+      }  // 若只有vnode的子节点存在
+      else if (isDef(ch)) {
+        /**
+       * 判断oldVnode是否有文本？
+       * 若没有，则把vnode的子节点添加到真实DOM中
+       * 若有，则清空Dom中的文本，再把vnode的子节点添加到真实DOM中
+       */
+        if (process.env.NODE_ENV !== 'production') {
+          checkDuplicateKeys(ch)
+        }
+        if (isDef(oldVnode.text)) nodeOps.setTextContent(elm, '')
+        addVnodes(elm, null, ch, 0, ch.length - 1, insertedVnodeQueue)
+      } else if (isDef(oldCh)) {
+        removeVnodes(oldCh, 0, oldCh.length - 1)
+      } else if (isDef(oldVnode.text)) {
+        nodeOps.setTextContent(elm, '')
+      }
+    } else if (oldVnode.text !== vnode.text) {
+      nodeOps.setTextContent(elm, vnode.text)
+    }
+    if (isDef(data)) {
+      if (isDef(i = data.hook) && isDef(i = i.postpatch)) i(oldVnode, vnode)
+    }
+  }
