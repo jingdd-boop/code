d716b72e32fd5baa5222042068d763b948527faa
jingdd-boop <1736929925@qq.com>
2021-07-16 16:14:29 +0800

    webpack初

---
+++ webpack/学习/7.16/index.js
@@ -0,0 +1,13 @@
+import Header from './header.js'
+import Sidebar from './sidebar.js'
+import Content from './content.js'
+
+
+// module.export = 
+// var Header= require('./header.js')
+
+new Header()
+new Sidebar()
+new Content()
+
+
